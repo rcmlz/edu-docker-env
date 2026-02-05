332af28a028196ee87330740c31f4ba360b72919
rcmlz <19784049+rcmlz@users.noreply.github.com>
2026-02-05 13:44:45 +0100

    Comment out Docker down command in launch.bat
    Comment out the command to bring down Docker containers.

---
+++ launch.bat
@@ -0,0 +1,13 @@
+echo off
+REM set DOCKER_COMPOSE_FILE_NAME=docker-compose-full.yml
+set DOCKER_COMPOSE_FILE_NAME=docker-compose-minimal.yml
+
+set HOME=%userprofile%
+set DOCKER_COMPOSE_FILE=%userprofile%\Downloads\%DOCKER_COMPOSE_FILE_NAME%
+set DOCKER_COMPOSE_FILE_URL=https://raw.githubusercontent.com/rcmlz/edu-docker-env/refs/heads/main/%DOCKER_COMPOSE_FILE_NAME%
+
+curl -o %DOCKER_COMPOSE_FILE% %DOCKER_COMPOSE_FILE_URL%
+
+docker compose -f %DOCKER_COMPOSE_FILE% up
+
+REM docker compose -f %DOCKER_COMPOSE_FILE% down --remove-orphans
